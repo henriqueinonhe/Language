0f3f0f27f7a9119804eb2403ca195a1982dc7af1
henriqueinonhe <henriqueinonhe@gmail.com>
2019-05-07 20:15:56 -0300

    Minor  Changes

--- Utils/pool.h
+++ Utils/pool.h
@@ -23,7 +23,7 @@ public:
 
     PoolRecordPointer<T> getPointer(const T &sample)
     {
-        //TODO Implement sorting ang binary search here!
+        //TODO Implement sorting and binary search here!
         //But as this is a small optimization, this should be delayed!
         /* Searches for already existing record containing the sample
          * and returns the record's pointer if it exists. */
